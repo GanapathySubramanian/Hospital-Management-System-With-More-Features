170c62068ca054ca17222060a3876ada7bd6a5bf
Ganapathy Subramanian S <60843507+ganapathyda@users.noreply.github.com>
2020-10-27 20:50:15 +0530

    Update appointmentdetails.php

--- appointmentdetails.php
+++ appointmentdetails.php
@@ -26,7 +26,7 @@ function filterTable($query)
 <html>
 <head>
 <title>Appointment Details</title>
-
+<link rel="shortcut icon" type="image/x-icon" href="images/em.jpg" />
     <link rel="stylesheet" href="loader.css">
    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
   <style>
