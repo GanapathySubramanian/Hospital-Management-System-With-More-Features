ea8b0fafc36276dd5309b871a3c7392e028bc478
Ganapathy Subramanian S <60843507+ganapathyda@users.noreply.github.com>
2020-11-03 20:29:08 +0530

    Update docprescription.php

--- docprescription.php
+++ docprescription.php
@@ -14,7 +14,7 @@ $fees=$_POST['cfees'];
 <html>
 <head>
 	<title>Prescription</title>
-	<link rel="shortcut icon" type="image/x-icon" href="images/plus.jpg" />
+	<link rel="shortcut icon" type="image/x-icon" href="images/em.jpg" />
 	<link rel="stylesheet" href="loader.css">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
